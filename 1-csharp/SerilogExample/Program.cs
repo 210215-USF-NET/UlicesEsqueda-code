083c1a106ae262f599bffc0e7683245ff059b14b
Ulices Esqueda <uesqueda756@gmail.com>
2021-02-24 12:14:27 -0800

    Added Serilog folders and program.

--- 1-csharp/SerilogExample/Program.cs
+++ 1-csharp/SerilogExample/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using Serilog;
-using Serilog.Formatting.Compact;
-using Serilog.Sinks.Json;
+// using Serilog.Formatting.Compact;
+// using Serilog.Sinks.Json;
+// using Serilog.Core;
+// using Serilog.Events;
+// using Serilog.Debugging;
+
 
 namespace SerilogExample
 {
@@ -10,10 +14,16 @@ namespace SerilogExample
         static void Main()
         {
             Log.Logger = new LoggerConfiguration()
-                .WriteTo.File(new CompactJsonFormatter(), "./logs/myapp.json")
+                .MinimumLevel.Verbose()
+                .WriteTo.File("logs/Logs.json")
                 .CreateLogger();
 
-            Log.Information("Hello, world!");
+            Log.Verbose("Verbose log message");
+            Log.Debug("Debug log message");
+            Log.Information("Information log message");
+            Log.Warning("Warning log message");
+            Log.Error("Error log message");
+            Log.Fatal("Fatal log message");
 
             int a = 10, b = 0;
             try
@@ -30,5 +40,39 @@ namespace SerilogExample
                 Log.CloseAndFlush();
             }
         }
+        
+        //My Code:
+        // static void Main()
+        // {
+        //     var levelSwitch = new LoggingLevelSwitch();
+
+        //     Log.Logger = new LoggerConfiguration()
+        //         .MinimumLevel.ControlledBy(levelSwitch)
+        //         .WriteTo.File(new CompactJsonFormatter(), "./logs/myapp.json")
+        //         .CreateLogger();
+
+        //     levelSwitch.MinimumLevel = LogEventLevel.Verbose;
+
+        //     Log.Information("Hello, world!");
+        //     Log.Verbose("This is a verbose call");
+        //     //Log.Error("This is an error.");
+        //     Log.Warning("Dont go there!");
+        //     Log.Verbose("Second rebose");
+
+        //     int a = 10, b = 0;
+        //     try
+        //     {
+        //         Log.Debug("Dividing {A} by {B}", a, b);
+        //         Console.WriteLine(a / b);
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Log.Error(ex, "Something went wrong");
+        //     }
+        //     finally
+        //     {
+        //         Log.CloseAndFlush();
+        //     }
+        // }
     }
 }
